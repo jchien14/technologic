58fd1a1c7c79a59cba52e43a09a7b45c22792672
Jonathan Chien <jchien10@gmail.com>
2014-09-06 23:01:43 -0400

    Print state

--- build/src/main/java/history/GalleryState.java
+++ build/src/main/java/history/GalleryState.java
@@ -1,6 +1,6 @@
 package history;
 
-import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
 
 import java.util.*;
 
@@ -10,6 +10,7 @@ import java.util.*;
 public final class GalleryState {
 
     private final int time;
+
     private final Set<String> employeesInside;
     private final Set<String> guestsInside;
     private final Map<String, PersonHistory> histories;
@@ -25,12 +26,11 @@ public final class GalleryState {
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder(Integer.toString(time));
-        final Joiner joiner = Joiner.on(HistoryUtils.LIST_DELIMITER);
 
         builder.append(HistoryUtils.DELIMITER);
-        builder.append(joiner.join(employeesInside));
+        builder.append(HistoryUtils.toListDelimitedString(employeesInside));
         builder.append(HistoryUtils.DELIMITER);
-        builder.append(joiner.join(guestsInside));
+        builder.append(HistoryUtils.toListDelimitedString(guestsInside));
         builder.append(HistoryUtils.DELIMITER);
         builder.append(PersonHistory.toString(histories));
 
@@ -50,6 +50,10 @@ public final class GalleryState {
         }
     }
 
+    /**
+     * No defensive copying for performance reasons. DO NOT MUTATE the returned List. Use
+     * methods {@link leaveRoom()} or {@link enterRoom()} instead.
+     */
     public List<Integer> getRoomsVisited(String name) {
         final PersonHistory history = histories.get(name);
         if (history == null) {
@@ -67,4 +71,31 @@ public final class GalleryState {
             return history.getTimeSpentInGallery();
         }
     }
+
+    public Set<String> getEmployeesInsideGallery() {
+        return employeesInside;
+    }
+
+    public Set<String> getGuestsInsideGallery() {
+        return guestsInside;
+    }
+
+    public Map<Integer, Set<String>> getRoomOccupancies() {
+        final Map<Integer, Set<String>> roomOccupancies = new TreeMap<Integer, Set<String>>();
+        for (PersonHistory history : histories.values()) {
+            if (history.isInsideRoom()) {
+                Integer room = Iterables.getLast(history.getRoomsVisited());
+                if (roomOccupancies.containsKey(room)) {
+                    roomOccupancies.get(room).add(history.getName());
+                } else {
+                    final Set<String> people = new TreeSet<String>();
+                    people.add(history.getName());
+                    roomOccupancies.put(room, people);
+                }
+            }
+        }
+        return roomOccupancies;
+    }
+
+    // TODO: Add methods for logappend to call (e.g. leaveRoom(person, room), enterRoom(person, room))
 }
